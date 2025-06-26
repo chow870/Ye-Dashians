cdd1670ba35ad06294a5e1da1e4f0ea5cc73169c
Anshu Gupta <anshugupta4142009@gmail.com>
2025-06-26 15:16:34 +0530

    Added change of origin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':"https://ye-dashians-backend.onrender.com",
-      '/maps':"https://ye-dashians-backend.onrender.com",
-
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ye-dashians-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/maps': {
+        target: 'https://ye-dashians-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
