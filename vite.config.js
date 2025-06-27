f0b578bb09f7e2ad059bed14ccb0d128559ad1f8
Anshu Gupta <anshugupta4142009@gmail.com>
2025-06-27 09:00:49 +0530

    removed vite proxy

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ye-dashians-backend.onrender.com',
-        changeOrigin: true,
-        secure: false, // use `false` if backend doesn't have a valid cert
-      },
-      '/maps': {
-        target: 'https://ye-dashians-backend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
